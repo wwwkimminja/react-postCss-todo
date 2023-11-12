dc2e533e3a747890b0e9953a0125a85410dd15fd
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-11-12 15:36:40 +0900

    feat : save todoList at localStorage

--- src/context/TodoListContext.jsx
+++ src/context/TodoListContext.jsx
@@ -1,12 +1,13 @@
-import { createContext, useReducer, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 import { todoReducer } from "../reducer/todo-reducer";
 
 export const TodoListContext = createContext();
 
 export function TodoListProvider({children }) {
 
+  const fetched = JSON.parse(localStorage.getItem("todoList"));
 
-  const [todoList,dispatch]=useReducer(todoReducer,fetched)
+  const [todoList,dispatch]=useReducer(todoReducer,fetched??[])
 
   const handleDelete = (id)=>{
     dispatch({type:'deleted',id})
@@ -18,6 +19,10 @@ export function TodoListProvider({children }) {
   const handleAdd = (content)=>{
     dispatch({type:"added",content})
   }
+
+  useEffect(()=>{
+      localStorage.setItem("todoList",JSON.stringify(todoList))
+  },[todoList])
 
   return (
     <TodoListContext.Provider value={{todoList,handleAdd,handleState,handleDelete}}>
